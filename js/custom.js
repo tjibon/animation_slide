21aa77199b7a42517c502999ce8599c07c3498ad
tjibon <41022171+tjibon@users.noreply.github.com>
2022-08-24 17:03:30 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+// ..............Custom JavaScript Code.................
+$(document).ready(function(){
+
+  $(".carousel-list").owlCarousel({
+
+    items: 1,
+    autoplay: false,
+    loop: true,
+     dots:false,
+     nav:true,
+     lazyLoad: true,
+     animateOut: 'fadeOut',
+     responsiveClass:true,
+     responsive:{
+         0:{
+             items:1,
+             nav:true,
+         },
+         600:{
+             items:1,
+             nav:true,
+         },
+         1000:{
+             items:1,
+             nav:true,
+             
+         }
+     }
+
+
+  });
+
+
+  /// wow js
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null,    // optional scroll container selector, otherwise use window,
+      resetAnimation: true,     // reset animation on end (default is true)
+    }
+  );
+  wow.init();
+
+
+});
